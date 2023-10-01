ab4e4e2e29d70a9b18103710981c4aee9eae5eb5
nicoas_rangel <nicolasrangelhopper@gmail.com>
2023-10-01 16:11:39 -0300

    aula_02

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ namespace ScreenSound
     {
         public static void Main(string[] args)
         {
+            Dictionary<string, List<int>> BandasCadastradas = new Dictionary<string, List<int>>();
             string apresentacao = "Olá! Seja Bem Vindo ao Screen Sound.";
+
 
             void ExibirLogo()
             {
@@ -44,9 +46,9 @@ namespace ScreenSound
                 {
                     case 1: CadastroDeBandas();
                         break;
-                    case 2: Console.WriteLine();
+                    case 2: ListarBandasCadastradas();
                         break;
-                    case 3: Console.WriteLine();
+                    case 3: AvaliarBanda();
                         break;
                     case 4: Console.WriteLine();
                         break;
@@ -72,7 +74,58 @@ namespace ScreenSound
                 ExibirTituloDaOpcao("CADASTRO DE BANDAS");
                 Console.Write("\nDigite o nome da banda que deseja cadastrar: ");
                 string nomeDaBanda = Console.ReadLine()!;
+                BandasCadastradas.Add(nomeDaBanda, new List<int>());
+                Console.WriteLine($"Banda {nomeDaBanda} foi cadastrada com sucesso!");
+                Thread.Sleep(1250);
+                Console.Clear();
+                MenuPrincipal();
+            }
 
+            void ListarBandasCadastradas()
+            {
+                Console.Clear();
+                ExibirTituloDaOpcao("APRESENTAÇÃO DE TODAS AS BANDAS CADASTRADAS");
+                
+                foreach (string banda in BandasCadastradas.Keys)
+                {
+                    Console.WriteLine($"Banda: {banda}");
+                }
+
+                Console.WriteLine("Digite qualquer tecla para voltar ao menu inicial");
+                Console.ReadKey();
+                Console.Clear();
+                MenuPrincipal();
+            }
+
+            void AvaliarBanda()
+            {
+                //digite qual banda deseja avaliar
+                // se a banda exitir no dicionario >> atribuir uma nota
+                // senão, volta ao menu principal
+
+                Console.Clear();
+                ExibirTituloDaOpcao("CHEGOU A HORA DE DAR A NOTA A BANDA");
+                Console.Write("Qual banda você quer avaliar? ");
+                string nomeDaBanda = Console.ReadLine()!;
+
+                if (BandasCadastradas.ContainsKey(nomeDaBanda))
+                {
+                    Console.Write($"Qual nota voçê acha que a banda {nomeDaBanda} merece? ");
+                    int notaDaBanda = int.Parse(Console.ReadLine()!);
+                    BandasCadastradas[nomeDaBanda].Add(notaDaBanda);
+                    Console.WriteLine($"A nota {notaDaBanda} foi registrada com sucesso para a banda {nomeDaBanda}");
+                    Console.WriteLine("Digite qualquer tecla para voltar ao menu inicial");
+                    Console.ReadKey();
+                    Console.Clear();
+                    MenuPrincipal();
+                } else
+                {
+                    Console.WriteLine("Sinto muito! a banda que voce informou não está cadastrada");
+                    Console.WriteLine("Digite qualquer tecla para voltar ao menu inicial");
+                    Console.ReadKey();
+                    Console.Clear();
+                    MenuPrincipal();
+                }
             }
 
             MenuPrincipal();
